a4e8fe22794da503e660191979941a33efa9e18f
i0Ek3 <kno30826@gmail.com>
2018-12-22 19:55:41 +0800

    Update cryptology.go

--- cryptology.go
+++ cryptology.go
@@ -5,6 +5,7 @@ import (
         "io"
         "log"
         "crypto/sha1"
+        "crypto/md5"
 )
 
 // Of course Google add more cryptology packages into Go languages to prevent attacks from hackers. 
@@ -20,7 +21,7 @@ type Hash interface {
 }
 
 
-func main() {
+func hasher() {
         hasher := sha1.New() // create a new object hash.Hash
         io.WriteString(hasher, "test")
         b := []byte{}
@@ -35,4 +36,31 @@ func main() {
         }
         checksum := hasher.Sum(b)
         fmt.Printf("checksum: %x\n", checksum)
+}
+
+func md5str() {
+        md5str := md5.New()
+        io.WriteString(md5str, "test")
+        b := []byte{}
+        fmt.Printf("hex: %x\n", md5str.Sum(b))
+        fmt.Printf("dec: %d\n", md5str.Sum(b))
+
+        md5str.Reset()
+        data := []byte("This sentence will be crypted!")
+        n, err := md5str.Write(data)
+        if n != len(data) || err != nil {
+                log.Printf("Hash write error: %v / %v", n, err)
+        }
+        checksum := md5str.Sum(b)
+        fmt.Printf("checksum: %x\n", checksum)
+    
+
+}
+
+
+func main() {
+        fmt.Printf("------------------hasher()-------------------\n")
+        hasher()
+        fmt.Printf("------------------md5str()-------------------\n")
+        md5str()
 }
