d7e87c91958afcdca92ac16f9de79795f5f231fe
i0Ek3 <kno30826@gmail.com>
2018-12-26 16:38:38 +0800

    Add error & panic

---
+++ panic.go
@@ -0,0 +1,32 @@
+package main 
+
+import (
+        "fmt"
+        "os"
+        //"errors"
+)
+
+var err error 
+
+func init() {
+    if err != nil {
+        panic("ERROR occurred:" + err.Error())
+    }
+}
+
+var user = os.Getenv("USER")
+
+func check() {
+        if user == "" {
+                panic("Unknown user: no value or $USER")
+    }
+}
+
+func main() {
+        check()
+        fmt.Println("Starting...")
+        panic("A server error occurred: Stop it now!")
+        fmt.Println("Ending...")
+}
+
+
