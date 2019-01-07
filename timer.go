f2f7d95dc6d3bf4bd20f12ebdf6a6f31c636a112
i0Ek3 <kno30826@gmail.com>
2019-01-07 11:05:08 +0800

    Add: ticker

---
+++ timer.go
@@ -0,0 +1,36 @@
+package main 
+
+import (
+        "fmt"
+        "time"
+)
+
+// Timer为定时器，Ticker为计时器
+//
+// time.Ticker对象以指定的时间间隔重复的向通道 C 发送时间值
+// type Ticker struct {
+//         C <- chan Time
+//         ....
+// }
+//
+// 结果通道是必须要带缓冲的
+// ch := make(chan int, 1) 
+
+
+
+func main() {
+        tick := time.Tick(1e8)
+        boom := time.After(5e8) // 只发送一次时间
+        for {
+                select {
+                case <- tick:
+                        fmt.Println("tick.")
+                case <- boom:
+                        fmt.Println("Boom!")
+                        return
+                default:
+                        fmt.Println("   .")
+                        time.Sleep(5e7)
+                }
+        }
+}
