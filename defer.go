bab5e59fec9a89f66675b4e004e8b70883ffcb93
i0Ek3 <kno30826@gmail.com>
2019-01-17 15:49:39 +0800

    Update defer

--- defer.go
+++ defer.go
@@ -8,13 +8,30 @@ import (
 
 func main() {
         // defer存在的意义在于释放某些已分配的资源,类似于c++中的auto关键字
-        fmt.Println("-------withDefer--------\n")
-        first()
-        fmt.Println("-------withNoDefer--------\n")
-        withNoDefer()
-        fmt.Println("-------new one--------\n")
-        data("newone")
+       // fmt.Println("-------withDefer--------\n")
+       // first()
+       // fmt.Println("-------withNoDefer--------\n")
+       // withNoDefer()
+       // fmt.Println("-------new one--------\n")
+       // data("newone")
+       defer_test()
 }
+
+func defer_test() {
+        var fs = [4]func(){}
+
+        for i := 0; i < 4; i++ {
+                defer fmt.Println("defer i = ", i)
+                defer func() { fmt.Println("defer_closure i = ", i) }()
+                fs[i] = func() { fmt.Println("closure i = ", i) }
+        }
+
+        for _, f := range fs {
+                f()
+        }
+}
+
+
 
 func withNoDefer() {
         fmt.Println("I am the first.\n")
