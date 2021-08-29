ebbabaffe2dbeecc0f62125a9716568f1db2f4f5
i0Ek3 <kno30826@gmail.com>
2021-08-30 00:00:29 +0800

    feat: Add mysql part

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/i0Ek3/go-learning
 
 go 1.16
 
-require github.com/i0Ek3/asrt v0.0.0-20210823161944-32ed0af35dc9
+require (
+	github.com/i0Ek3/asrt v0.0.0-20210823161944-32ed0af35dc9
+	github.com/jinzhu/gorm v1.9.16 // indirect
+)
---
+++ mysql/mysql.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"os"
+    "database/sql"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+    user    = "root"
+    pass    = ""
+    host    = "127.0.0.1"
+    port    = 3306
+    db      = "test"
+)
+
+func init() {
+    name, _ := os.Hostname()
+    if name == "hyena" {
+        pass = ""
+    }
+}
+
+func ConnectByGorm() (*gorm.DB, error) {
+    dns := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, pass, host, port, db)
+    return gorm.Open("mysql", dns)
+}
+
+func ConnectByRaw() (*sql.DB, error) {
+    dns := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, pass, host, port, db)
+    return sql.Open("mysql", dns)
+}
+
+func main() {
+    db, err := ConnectByGorm() // ConnectByRaw()
+    defer db.Close()
+    if err != nil {
+        panic(err)
+    }
+    err = db.Exec("SELECT CURRENT_TIMESTAMP();").Error
+    if err != nil {
+        panic(err)
+    }
+}
