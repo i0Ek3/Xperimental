ceda0fbc52d6c07886f7d4c32aa26a86d20055e9
i0Ek3 <kno30826@gmail.com>
2022-09-24 16:23:41 +0800

    doc(Go): update test.go

--- Go/test.go
+++ Go/test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-    test2()
+    test4()
 }
 
 func test1() {
@@ -23,4 +23,30 @@ func test2() {
     } else {
         fmt.Println(int(y))
     }
+}
+
+func test3() {
+    s := []int{5}
+	s = append(s, 6)
+	s = append(s, 7)
+	x := append(s, 11)
+	fmt.Println(s, x)
+	fmt.Println("------ x was overrided ------")
+	y := append(s, 12)
+	fmt.Println(s, x, y)
+}
+
+func test4() {
+    c := make(chan int, 1)
+	for done := false; !done; {
+		select {
+		default:
+			fmt.Println(1)
+		case <-c:
+			fmt.Println(2)
+			c = nil
+		case c <- 1:
+			fmt.Println(3)
+		}
+	}
 }
